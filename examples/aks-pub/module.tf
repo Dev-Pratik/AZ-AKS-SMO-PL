874d7facdf5990306d4a49e51d49df7b04c6d8a5
Dev-Pratik <87895044+Dev-Pratik@users.noreply.github.com>
2024-09-10 12:01:12 +0530

    Update module.tf

--- examples/aks-pub/module.tf
+++ examples/aks-pub/module.tf
@@ -4,10 +4,10 @@ module "aks_smo"{
     resource_group_name     = "rg-aks-smo"
     virtual_network_name    = "vnet-aks-1"
     vnet_address_space      = ["10.0.0.0/16"]
-    subnet_id               = "/subscriptions/739f2744-f1cc-4a85-978e-71f83a5145e5/resourceGroups/pbtest-rg/providers/Microsoft.Network/virtualNetworks/rbtest/subnets/default"
+    
   
-  #  subnet_name             = "sub-aks-1"
-  #  subnet_address_prefixes = ["10.0.1.0/24"]
+    subnet_name             = "sub-aks-1"
+    subnet_address_prefixes = ["10.0.1.0/24"]
 
     subnets = {
     subnet-3 = {
